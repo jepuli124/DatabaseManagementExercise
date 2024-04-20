99c9fc4f9fb41ffafa435d9dd2c488051223ab7d
Lauri Heiskanen <106094206+Lauri-Heiskanen@users.noreply.github.com>
2024-04-20 20:51:16 +0300

    added constraints and changed all salaries to 1001

---
+++ constraints.sql
@@ -0,0 +1,20 @@
+ALTER TABLE customer
+ALTER COLUMN email SET NOT NULL;
+
+ALTER TABLE project
+ALTER COLUMN p_start_date SET NOT NULL;
+
+-- change all salaries from 0 to 1001 to not get errors
+UPDATE employee
+SET salary = 1001;
+
+ALTER TABLE employee
+ADD CONSTRAINT salary_check CHECK (salary > 1000);
+
+
+
+
+
+
+
+
