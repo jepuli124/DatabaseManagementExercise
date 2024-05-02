925bb7e1ad8ad3dea9567d447a0917fa68b3e27a
jepuli124 <konsta.jalkanen.kj@gmail.com>
2024-05-02 14:43:12 +0300

    added the last role

--- roles.sql
+++ roles.sql
@@ -12,3 +12,5 @@ create or replace view traineeView
 as select e_id, emp_name, email from employee;
 
 GRANT SELECT ON traineeView TO trainee;
+
+GRANT SELECT ON employees_on_project, employees_in_department_in_hq, employees_by_skill, customers_by_location, employees_by_group, employees_by_title TO views_only;
